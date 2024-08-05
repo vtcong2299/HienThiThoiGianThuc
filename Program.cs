b86d17826b3dda2db03744c5c02022ff325dcd7f
vtcong2299 <vtcong2299@gmail.com>
2024-08-05 21:57:41 +0700

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+
+namespace HIENTHITHOIGIANTHUC
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            DateTime localTime=DateTime.Now;
+            Console.WriteLine("Datatime now is: {0}",localTime);
+        }
+    }
+}
